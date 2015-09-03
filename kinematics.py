aa7de4a3b77ed0783b0505c5575bd6a65bc685d9
joshwarren <joshua.warren@physics.ox.ac.uk>
2015-09-03 17:52:01 +0100

    Final working code
    Realised that gaussian is not a good fit - so idea of using it to find Re is not helpful.
    Have tidied code up: should only need to edit top few lines for other galaxies. Is redshift recorded anywhere so that it can be automatically entered?
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -18,11 +18,20 @@ wav_range=None
 
 
 galaxy = "ngc3557"
+z = 0.03 # redshift of galaxy
 discard = 2 # rows of pixels to discard- must have been the same 
             #    for all routines 
-wav_range="4200-"
-plots=False
+wav_range = "4200-"
+corrections = [[13,9],[35,1]]
 
+plots = False
+find_Re = False # fits radial profile with a gaussian
+
+
+def spxToKpc(x, z):
+    H0 = 70.4 # Mpc / kms^-1
+    val =  3*10**5 * z/H0 *10**3 * x * 0.67*4.85*10**(-6)
+    return val
 
 
 
@@ -74,12 +83,16 @@ galaxy_data /= np.median(galaxy_data)
 
 #print galaxy_data[12:15,8:11]
 #print galaxy_data[34:,0:3]
-galaxy_data[13,9]=1
-galaxy_data[35,1]=1
-galaxy_data_error[13,9]=1
-galaxy_data_error[35,1]=1
+#galaxy_data[13,9]=np.median(galaxy_data)
+#galaxy_data[35,1]=np.median(galaxy_data)
+#galaxy_data_error[13,9]=np.median(galaxy_data_error)
+#galaxy_data_error[35,1]=np.median(galaxy_data_error)
 #print np.min(galaxy_data)
 #print np.max(galaxy_data)
+for i in range(len(corrections)):
+    galaxy_data[corrections[i][0],corrections[i][1]] = np.median(galaxy_data)
+    galaxy_data_error[corrections[i][0],corrections[i][1]] = \
+        np.median(galaxy_data_error)
 
 # ------------============= Fit photometry ===============----------
 f = find_galaxy(galaxy_data, quiet=True, plot=plots)
@@ -100,30 +113,20 @@ print "PA_photo:" + str(90-f.theta) #+ "+/-" + str(abs(f.theta-f_err.theta))
 x, y, bin_num, xBin, yBin = np.loadtxt(tessellation_File, unpack=True, 
     skiprows = 1) 
 
-
 xBar, yBar = np.loadtxt(tessellation_File2, unpack=True, 
     skiprows = 1) 
 
-
-#xBar += -np.median(xBar)
-#yBar += -np.median(yBar)
-xBar += -36/2
-yBar += -36/2
-
-
+xBar += -(40.0-2*discard)/2.0
+yBar += -(40.0-2*discard)/2.0
 
 v_field = np.loadtxt(output_v, unpack=True)
 v_field -= np.median(v_field)
 
 
 
-
-
 # ------------============== Fit kinemetry ===============----------
 k = fit_kinematic_pa(xBar, yBar, v_field, quiet=True, plot=plots) 
 print "PA_kin:" + str(k[0]) + "+/-" + str(k[1]/3)
-
-
 
 
 # ------------============== Misalignment ================----------
@@ -136,17 +139,15 @@ mis = math.degrees(mis)
 print "Psi:" + str(mis)
 
 
-
-
-
 # ------------================= Lambda_R ================----------
+# Reload v_field and undo changes above
+xBar += (40.0-2*discard)/2.0
+yBar += (40.0-2*discard)/2.0
+v_field = np.loadtxt(output_v, unpack=True)
+
 r = np.sqrt(np.square(xBar-f.xmed)+np.square(yBar-f.ymed))
 sigma = np.loadtxt(output_sigma, unpack=True)
 
-# Reload v_field and undo changes above
-xBar += 36/2
-yBar += 36/2
-v_field = np.loadtxt(output_v, unpack=True)
 
 order = np.argsort(r)
 
@@ -160,69 +161,72 @@ for i in range(len(v_field)):
 
 
 # NB: lam is ordered in terms of increasing R.
-lam_num = flux_b[order]*r[order]*abs(v_field[order])
-lam_dom = flux_b[order]*r[order]*np.sqrt(np.square(v_field[order]) + np.square(sigma[order]))
+lam_num = flux_b[order]*r[order]*abs(v_field[order]) # numerator
+lam_den = flux_b[order]*r[order]*np.sqrt(np.square(v_field[order]) + 
+    np.square(sigma[order])) # denominator
 
 # cumulative summation
 lam_num = np.cumsum(lam_num)
-lam_dom = np.cumsum(lam_dom)
+lam_den = np.cumsum(lam_den)
 
+lam = lam_num/lam_den
 
-lam = lam_num/lam_dom
 if plots: 
     plt.title(r"Radial $\lambda_R$ profile")
-    plt.xlabel("Radius")
+    plt.xlabel("Radius (kpc)")
     plt.ylabel(r"$\lambda_R$")
-    plt.plot(r[order], lam)
+    plt.plot(spxToKpc(r[order],z), lam)
     plt.show()
-
-
 
 
 # ------------============= Effective Radius =============----------
 ## Fit galaxy radial flux profile with Gaussian and find half-light. 
 ## How close does this correspond to half of flux in field of view?
+if find_Re:
+    r = ((x-f.xmed)/abs(x-f.xmed)) * np.sqrt(np.square(x-f.xmed) + 
+        np.square(y-f.ymed))
+    order = np.argsort(r)
+   
+    x=np.int_(x)
+    y=np.int_(y)
+
+    xaxis = r[order]
+    yaxis = galaxy_data[x[order],y[order]]
+    error = galaxy_data_error[x[order],y[order]]
 
 
+    def gaussian(x, a, c):
+        val = a * np.exp(-(x)**2 / c**2)
+        return val
 
+    yaxis += -min(yaxis)
+    #yaxis[612] = 15
 
+    popt,pcov = curve_fit(gaussian, xaxis, yaxis, sigma = np.sqrt(error))
 
+    p = [1.0/10.0, 0.0, -1.0/3.0, 0.0, 1.0, -math.sqrt(math.pi)/4.0]
+    roots = np.roots(p)
+    Re = popt[1] * np.real(roots[-1])
+    Re_error = np.sqrt(pcov[1, 1]) * np.real(roots[-1])
+    
+    Re_kpc = spxToKpc(Re, z)
+    Re_error_kpc = spxToKpc(Re_error, z)
+    
 
-r = ((x-f.xmed)/abs(x-f.xmed))*np.sqrt(np.square(x-f.xmed)+np.square(y-f.ymed))
-order = np.argsort(r)
-
-
-
-#xaxis = np.concatenate((-r[order[::-1]], r[order]))
-#yaxis = np.concatenate((galaxy_data[x[order[::-1]],y[order[::-1]]], galaxy_data[x[order],y[order]]))
-xaxis = r[order]
-yaxis = galaxy_data[x[order],y[order]]
-error = np.concatenate((galaxy_data_error[x[order[::-1]],y[order[::-1]]], galaxy_data_error[x[order],y[order]]))
-
-
-def gaussian(x, a, c):
-    val = a * np.exp(-(x)**2 / c**2)
-    return val
-
-yaxis += -min(yaxis)
-
-#yaxis[612] = 15
-popt,pcov = curve_fit(gaussian, xaxis, yaxis)#, sigma = np.sqrt(error))
-
-Re = popt[1] * math.sqrt(math.pi)/4
-Re_error = np.sqrt(pcov[1, 1]) * math.sqrt(math.pi)/4
-
-print("Scale =  %.3f +/- %.3f" % (popt[0], np.sqrt(pcov[0, 0])))
-print("Sigma =  %.3f +/- %.3f" % (popt[1], np.sqrt(pcov[1, 1])))
-print "Re = %.3df +/- %.3f" % (Re, Re_error)
-
-
-plt.plot(xaxis,yaxis, 'y')
-xm = np.linspace(-30., 30., 100)  # 100 evenly spaced points
-plt.plot(xm, gaussian(xm, popt[0], popt[1]), 'r')
-plt.axvline(Re)
-plt.axvline(-Re)
-plt.show()
-
-
+    #print("Scale =  %.3f +/- %.3f" % (popt[0], np.sqrt(pcov[0, 0])))
+    #print("Sigma =  %.3f +/- %.3f" % (popt[1], np.sqrt(pcov[1, 1])))
+    #print "Re = %.3f +/- %.3f" % (Re, Re_error)
+    print "Re = (%.3f +/- %.3f)kpc" % (Re_kpc, Re_error_kpc)
+    
+    #plots = True
+    if plots:
+        plt.title("Radial flux profile")
+        plt.xlabel("Radius (kpc)")
+        plt.ylabel('Flux (normalised)')
+        plt.plot(spxToKpc(xaxis,z),yaxis, 'y')
+        xm = np.linspace(-30., 30., 100)  # 100 evenly spaced points
+        plt.plot(spxToKpc(xm,z), gaussian(xm, popt[0], popt[1]), 'r')
+        plt.axvline(Re_kpc)
+        plt.axvline(-Re_kpc)
+        plt.show()
 
