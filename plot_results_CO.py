44a2d9d0eb98cbf5e72a5ac1ba9ba8e4dec4db03
joshwarren <joshua.warren@physics.ox.ac.uk>
2016-05-05 17:50:15 +0100

    EoDC: Updated full_analysis2.py
    Updated full_analysis2.py to reflect the additional keywork in plot_results.py, CO.
    
    plot_results_CO.py has been deleted as it has been superseded by the CO keyword in plot_results.py (see previous commit today).
    
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   full_analysis2.py
    	deleted:    plot_results_CO.py

--- plot_results_CO.py
+++
@@ -1,4 +0,0 @@
-#############################################################################
-## THIS ROUTINE HAS BE SUPERSEDED BY PLOT_RESULTS.PY AS THE FUNCTIONALLITY ##
-## OF THIS ROUTINE IMPLIMENTED BY THE CO KEYWORD.                          ##
-#############################################################################
