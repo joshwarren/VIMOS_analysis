Corrected equivalent width calculation
KDC_pop.py: as above

Bin.py: No change

 On branch master
 Changes to be committed:
	modified:   Bin.py
	modified:   KDC_pop.py

Bin.py
@@ -556,7 +556,7 @@ class Bin(object):
 		a = [min(np.where(self.lam > self.__parent__.common_range[0])[0]),
 			max(np.where(self.lam < self.__parent__.common_range[1])[0])]
 		return np.trapz(self.spectrum[a[0]:a[1]], x=self.lam[a[0]:a[1]]
-			)/self.n_spaxels_in_bin
+			)#/self.n_spaxels_in_bin
 
 	@property
 	def xBar(self):

KDC_pop.py
@@ -142,7 +142,10 @@ def KDC_pop(galaxy):
 	f = pop.fig
 	ax = pop.ax
 	OIII_pos = np.argmin(np.abs(pp.lam - 5007))
-	OIII_eqw_gals[i_gal] = pop.continuum[OIII_pos]/pop.e_line_spec[OIII_pos]
+	peak_width = 15
+	OIII_eqw_gals[i_gal] = np.trapz(pop.e_line_spec[OIII_pos - peak_width:OIII_pos + 
+		peak_width], x=pp.lam[OIII_pos - peak_width:OIII_pos + peak_width])/\
+		pop.continuum[OIII_pos]
 	del pop
 
 	# Outside apperture